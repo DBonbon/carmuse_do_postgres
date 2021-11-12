75d19c520d79d911fb9a99b8b90ed757659a6609
Dbonbon <yrfriedman@gmail.com>
2021-11-12 22:46:47 +0000

    aws codingfoentrepreneurs

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -174,21 +174,23 @@ STATICFILES_DIRS = [
 # See https://docs.djangoproject.com/en/3.2/ref/contrib/staticfiles/#manifeststaticfilesstorage
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / "staticfiles-cdn" # in production, we want cdn
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / "staticfiles-cdn" / "uploads"
 MEDIA_URL = '/media/'
 
-
+from .cdn.conf import * # noqa
 #AWS DO SETTINGS
-AWS_ACCESS_KEY_ID=os.getenv("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY=os.getenv("AWS_SECRET_ACCESS_KEY")
-AWS_STORAGE_BUCKET_NAME=os.getenv("AWS_STORAGE_BUCKET_NAME")
-AWS_S3_ENDPOINT_URL=os.getenv("AWS_S3_ENDPOINT_URL")
+#AWS_ACCESS_KEY_ID=os.getenv("AWS_ACCESS_KEY_ID")
+#AWS_SECRET_ACCESS_KEY=os.getenv("AWS_SECRET_ACCESS_KEY")
+#AWS_STORAGE_BUCKET_NAME=os.getenv("AWS_STORAGE_BUCKET_NAME")
+#AWS_S3_ENDPOINT_URL=os.getenv("AWS_S3_ENDPOINT_URL")
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+#DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 #AWS_S3_OBJECT_PARAMETERS={"CacheControl": "mas-age=86400",}
 # Wagtail settings
 
